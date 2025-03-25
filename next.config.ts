4317a742d5f2e111d2a34a77cc1494d9bd31f7f0
Ian <ian@mobilewireless.tech>
2025-03-24 22:23:08 -0700

    basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
+  basePath: '/woogle',
+  assetPrefix: '/woogle/',
 };
 
 export default nextConfig;
